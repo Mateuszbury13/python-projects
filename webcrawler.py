231438c0eacca3cdc1958e3b1ba6dcad7dd1bf79
Mateuszbury13 <mateuszbury@poczta.onet.pl>
2020-04-01 18:40:39 +0200

    implemented fetching the whole document

--- webcrawler.py
+++ webcrawler.py
@@ -6,3 +6,14 @@ Created on Wed Apr  1 16:51:07 2020
 @author: admin
 """
 
+import urllib.request
+from bs4 import BeautifulSoup
+
+print('Getting staff urls...')
+
+staff_url = 'http://wa.amu.edu.pl/wa/pl/staff_list'
+response = urllib.request.urlopen(staff_url)
+data = response.read()
+doc = BeautifulSoup(data, 'html.parser')
+
+print(doc)
