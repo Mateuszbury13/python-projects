bc8bc9932c78b7ff820d5b83e00c409692a6f61e
Mateuszbury13 <mateuszbury@poczta.onet.pl>
2020-04-09 10:09:23 +0200

    Multithreaded approach

--- webcrawler.py
+++ webcrawler.py
@@ -10,6 +10,7 @@ import urllib.request
 from bs4 import BeautifulSoup
 import urllib.parse
 from datetime import datetime
+from multiprocessing import Pool
 
 def main():
     print('Getting staff urls...')
@@ -29,6 +30,11 @@ def main():
             urls.append(encoded_url)
         
     print('Staff emails found:')
+    
+    mypool = Pool()
+    mypool.map(get_details, urls)
+    
+    
     for url in urls:
         print(get_details(url))
         
@@ -45,6 +51,8 @@ def get_content(url):
     doc = BeautifulSoup(data, 'html.parser')
     return doc.find(id='tresc_wlasciwa')
 
+def print_details(url):
+    print(get_details(url))
 
 def get_details(url):
     try:
