9253ef2e050c3722c3f4e03e63a0e8de5610005f
Mateuszbury13 <mateuszbury@poczta.onet.pl>
2020-04-15 16:11:46 +0200

    implemented translate library

--- translate.py
+++ translate.py
@@ -6,3 +6,11 @@ Created on Wed Apr 15 16:02:07 2020
 @author: admin
 """
 
+from googletrans import Translator
+
+def main():
+    translator = Translator()
+    result = translator.translate("Jak się masz?", dest='de')
+    print(result.text)
+
+main()
